71c3be12c097c1f1a796857e4fd89a186a92b1f2
Aleksey <allenb495@gmail.com>
2024-10-05 15:33:42 +0700

    script for extracting answers

---
+++ script/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+df = pd.read_csv('Answers/Answers.csv', encoding='ISO-8859-1')
+
+end = 10**6
+df = df[0:end]
+
+
+def clean_html(html):
+    return BeautifulSoup(html, 'html.parser').get_text(separator=' ', strip=True)
+
+
+df['Body'] = df['Body'].apply(clean_html)
+
+# print(df.Body.head(5))
+df.to_csv('output.csv', index=False)
